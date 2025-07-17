98c56cf525b77e6809728d2231a6d5b5564bbaa0
unknown <TeamGranon@P3680-1-SG.NEUROPSI.cnrs-gif.fr>
2025-07-17 11:40:49 +0200

    correcting errors

---
+++ track_all_videos.ps1
@@ -0,0 +1,16 @@
+$videos = Get-ChildItem "for_tracking\*" -Include *.mp4, *.mkv
+
+foreach ($video in $videos) {
+    Write-Host "Procesando: $($video.Name)" -ForegroundColor Cyan
+
+    sleap-track `
+        -m "SLEAP_track\models\250702_114533.centroid.n=765" `
+        -m "SLEAP_track\models\250702_132640.centered_instance.n=765" `
+        --tracking.tracker flow `
+        --tracking.similarity centroid `
+        --tracking.match hungarian `
+        --tracking.max_tracks 2 `
+        -n 2 `
+        -o "tracked_videos" `
+        $video.FullName
+}
